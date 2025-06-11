5593a4e1b368ff8b030ec1d856e09e69aa29245b
Swan <stbnbasson@gmail.com>
2025-06-11 20:03:19 +0200

    feat: tcg_trouver_emplacement_classeur.py montre le numéro de la double page + recto ou verso

--- tcg/tcg_trouver_emplacement_classeur.py
+++ tcg/tcg_trouver_emplacement_classeur.py
@@ -1,24 +1,34 @@
+import math
+
 def trouver_page_et_emplacement(numero_carte):
 	if numero_carte < 1:
 		return "Le numéro doit être supérieur ou égal à 1."
+
 	page = (numero_carte - 1) // 9 + 1
 	position = (numero_carte - 1) % 9 + 1
-	return page, position
+	vraie_page = math.ceil(page / 2)
+	face = "recto" if page % 2 == 0 else "verso"
+
+	return page, position, vraie_page, face
 
 def main():
 	while True:
 		entree = input("Entrez le numéro de la carte (ou 'exit' pour quitter) : ").strip().lower()
+
 		if entree == "exit":
 			print("Fermeture du programme.")
 			break
+
 		if not entree.isdigit():
 			print("Veuillez entrer un numéro valide.")
 			continue
+
 		numero = int(entree)
 		resultat = trouver_page_et_emplacement(numero)
+
 		if isinstance(resultat, tuple):
-			page, position = resultat
-			print(f"\tCarte n°{numero} → Page {page}, Position {position}/9\n")
+			page, position, vraie_page, face = resultat
+			print(f"\tCarte n°{numero} → Page {page} ({vraie_page}, {face}), Position {position}/9\n")
 		else:
 			print(resultat)
 
